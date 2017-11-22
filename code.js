038da7b7a5eadb252105f8f815a23e40229d97aa
Hanna Sköld <hanna_skold@hotmail.com>
2017-11-22 13:11:18 +0100

    nya filer

--- code.js
+++ code.js
@@ -1,13 +1,30 @@
+let movieData = require("./data");
+let $ = require("jquery");
+
 function renderMovie (movie){
 
-    document.getElementById("image").src = movie.img;
-    document.getElementById("movieTitle").innerText = movie.title;
-    document.getElementById("description").innerText = movie.description;
+    $("#image").attr("src", movie.img);
+    $("#movieTitle").text(movie.title);
+    $("#description").text(movie.description);
 
     let html = "";   
     for (let i = 0; i < movie.list.length; i++) {
         html += "<li>" + movie.list[i] + "</li>";        
      };   
-     document.getElementById("list").innerHTML = html;
+     $("#list").append(html);
 };
-    renderMovie(movieData)
+
+function changeStarRating(rating){
+    $(".filled").removeClass("filled");
+    for(let i=1; i<=rating; i++){
+     $("[data-rating-id=" + i + "]").addClass("filled");     
+  }
+}
+
+$(".stars").on("click", "span", function(e){
+    let star = $(e.target);
+    let rating = parseInt(star.attr("data-rating-id"));
+    changeStarRating(rating)
+});
+
+renderMovie(movieData)
--- data.js
+++ data.js
@@ -1,7 +1,8 @@
-let movieData = {
-    title: "Titanic",
-    description: "Filmen handlar om de två uppdiktade passagerarna Jack Dawson och Rose DeWitt Bukater ombord på RMS Titanic som blir förälskade i varandra och kämpar för att överleva den katastrofalaförlisningen av fartyget.", 
-    list: ["1997", "Leonardo DiCaprio", "Kate Winslet" ],
-    img: "http://cdn.collider.com/wp-content/uploads/titanic-movie-poster-01.jpeg"
+module.exports = {
+    title: 'Titanic',
+    description: 'Filmen handlar om de två uppdiktade passagerarna Jack Dawson och Rose DeWitt Bukater ombord på RMS Titanic som blir förälskade i varandra och kämpar för att överleva den katastrofalaförlisningen av fartyget.', 
+    list: ['1997', 'Leonardo DiCaprio', 'Kate Winslet' ],
+    img: 'http://cdn.collider.com/wp-content/uploads/titanic-movie-poster-01.jpeg'
 };
+
 
